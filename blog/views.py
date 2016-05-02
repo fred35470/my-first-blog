86f274f2644e83ad8c1e3b47aa1d80a6647e8750
fred <fsigonneau@free.fr>
2016-05-02 14:38:58 +0200

    Ajout de la liste des brouillons

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,8 @@ from .forms import PostForm
 # Create your views here.
 
 def post_list(request):
-    #posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    posts=Post.objects.all()
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #posts=Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
@@ -20,7 +20,7 @@ def post_new(request):
         if form.is_valid():
             post=form.save(commit=False)
             post.author=request.user
-            post.published_date=timezone.now()
+            #post.published_date=timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
@@ -41,3 +41,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts=Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request,'blog/post_draft_list.html', {'posts': posts})
+
