d8d9e79712953c93978930d826f9adc3ab142ced
fred <fsigonneau@free.fr>
2016-05-02 16:21:14 +0200

    Ajout publication et suppression de posts

--- blog/views.py
+++ blog/views.py
@@ -10,9 +10,11 @@ def post_list(request):
     #posts=Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+
 def post_detail(request, pk):
     post=get_object_or_404(Post,pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method=="POST":
@@ -42,7 +44,19 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def post_draft_list(request):
     posts=Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request,'blog/post_draft_list.html', {'posts': posts})
 
+
+def post_publish(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail',pk=pk)
+
+
+def post_remove(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
